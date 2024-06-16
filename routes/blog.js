00f122d9bf1c6926025fb7deaa0d0b487c86ce4f
Nishant Chauhan <nishant031102@gmail.com>
2024-06-16 18:16:36 +0530

    You can comment on the blog added on the blog id page

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,8 @@ const multer = require("multer");
 const router = Router();
 const path = require("path");
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -31,6 +33,16 @@ router.get("/:id", async (req, res) => {
     blog,
   });
 });
+
+router.post('/comment/:blogId',async (req,res) =>{
+  const comment = await Comment.create({
+    content:res.body.content,
+    blogId:req.params.blogId,
+    createdBy:req.user._id,
+  })
+
+  return res.redirect(`/blog/${req.params.blogId}`)
+})
 
 router.post("/", upload.single("coverImage"), async (req, res) => {
   const { title, body } = req.body;
